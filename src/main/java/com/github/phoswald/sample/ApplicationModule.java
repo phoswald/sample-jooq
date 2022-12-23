dd5f2de2f2f1b9bcc46a1e1f6eaf644cfa174754
Philip Oswald <philip.oswald@gmail.com>
2022-12-23 21:47:03 +0100

    Refactor application module

--- src/main/java/com/github/phoswald/sample/ApplicationModule.java
+++ src/main/java/com/github/phoswald/sample/ApplicationModule.java
@@ -12,41 +12,42 @@ import com.github.phoswald.sample.task.TaskRepository;
 import com.github.phoswald.sample.task.TaskResource;
 import com.github.phoswald.sample.utils.ConfigProvider;
 
-public interface ApplicationModule {
+public class ApplicationModule {
 
-    default Application getApplication() {
+    public Application getApplication() {
         return new Application(getConfigProvider(), //
                 getSampleResource(), getSampleController(), getTaskResource(), getTaskController());
     }
 
-    default ConfigProvider getConfigProvider() {
+    public ConfigProvider getConfigProvider() {
         return new ConfigProvider();
     }
 
-    default SampleResource getSampleResource() {
+    public SampleResource getSampleResource() {
         return new SampleResource(getConfigProvider());
     }
 
-    default SampleController getSampleController() {
+    public SampleController getSampleController() {
         return new SampleController(getConfigProvider());
     }
 
-    default TaskResource getTaskResource() {
+    public TaskResource getTaskResource() {
         return new TaskResource(getTaskRepositoryFactory());
     }
 
-    default TaskController getTaskController() {
+    public TaskController getTaskController() {
         return new TaskController(getTaskRepositoryFactory());
     }
 
-    default Supplier<TaskRepository> getTaskRepositoryFactory() {
+    public Supplier<TaskRepository> getTaskRepositoryFactory() {
         return () -> new TaskRepository(getConnection());
     }
 
-    default Connection getConnection() {
+    public Connection getConnection() {
         try {
             var config = getConfigProvider();
-            String url = config.getConfigProperty("app.jdbc.url").orElse("jdbc:h2:mem:test " + hashCode() + " ;DB_CLOSE_DELAY=-1;INIT=RUNSCRIPT FROM 'src/main/resources/schema.sql'");
+            String url = config.getConfigProperty("app.jdbc.url").orElse("jdbc:h2:mem:test " + hashCode()
+                    + " ;DB_CLOSE_DELAY=-1;INIT=RUNSCRIPT FROM 'src/main/resources/schema.sql'");
             String username = config.getConfigProperty("app.jdbc.username").orElse("sa");
             String password = config.getConfigProperty("app.jdbc.password").orElse("sa");
             return DriverManager.getConnection(url, username, password);
